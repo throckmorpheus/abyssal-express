b2f1ac2be0bdb2bfc0b66e1ff498dcf5c422dafa
Throckmorpheus <finnorelsemccormick@gmail.com>
2024-11-03 16:51:41 +0000

    Set up script to control dialogue portrait

---
+++ src/DialogueController.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+using YarnSpinnerGodot;
+
+public partial class DialogueController : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		portraitRect = GetNode<TextureRect>("CharacterPortrait/TextureRect");
+		portraitRect.Texture = CharacterPortrait;
+		portraitRect.Visible = true;
+
+		dialogueRunner = GetNode<DialogueRunner>("DialogueRunner");
+		dialogueRunner.Connect("onNodeStart", new Callable(this, MethodName.OnDialogueStart));
+		dialogueRunner.Connect("onDialogueComplete", new Callable(this, MethodName.OnDialogueComplete));
+	}
+
+	public void OnDialogueStart() {
+		portraitRect.Visible = true;
+	}
+
+	public void OnDialogueComplete() {
+		portraitRect.Visible = false;
+	}
+
+	private Texture2D _characterPortrait;
+	[Export] public Texture2D CharacterPortrait
+	{
+		get => _characterPortrait;
+		set {
+			_characterPortrait = value;
+			portraitRect.Texture = _characterPortrait;
+		}
+	}
+
+	private TextureRect portraitRect;
+	private DialogueRunner dialogueRunner;
+}
